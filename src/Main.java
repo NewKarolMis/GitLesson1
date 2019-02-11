11a694abd0ffc40d8eb7cdfb43a3fe0b3c78e9b2
NewKarolMis <karol.mis.m@gmail.com>
2019-02-11 21:53:35 +0100

    hello git

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("a więc działasz");
+        System.out.println("a więc działasz, tylko nie tak jak powinieneś");
     }
 }
